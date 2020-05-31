b7db8a5a6a9495d85f3ca84925fdfb624e168585
Agus Supriyatna <agus@anterin.id>
2020-05-31 21:49:32 +0700

    View Booking

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,12 @@ const dashboardController = require('../controllers/admin/dashboardController')
 const categoryController = require('../controllers/admin/categoryController')
 const bankController = require('../controllers/admin/bankController')
 const itemController = require('../controllers/admin/itemController')
+const bookingController = require('../controllers/admin/bookingController')
 
 router.get('/', dashboardController.index)
 router.get('/categories', categoryController.index)
 router.get('/banks', bankController.index)
 router.get('/items', itemController.index)
+router.get('/booking', bookingController.index)
 
 module.exports = router
