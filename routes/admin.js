66bf2f819183b71f6f51690491baeabf8bdb76c5
Agus Supriyatna <agus@anterin.id>
2020-05-31 21:28:29 +0700

    View Category

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const dashboardController = require('../controllers/admin/dashboardController')
+const categoryController = require('../controllers/admin/categoryController')
 
 router.get('/', dashboardController.index)
+router.get('/categories', categoryController.index)
 
 module.exports = router
