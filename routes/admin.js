050c168be20fe1887b88eb0a8c294b97d41ef22e
Agus Supriyatna <agus@anterin.id>
2020-05-31 21:40:45 +0700

    View Item

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,11 @@ const router = require('express').Router()
 const dashboardController = require('../controllers/admin/dashboardController')
 const categoryController = require('../controllers/admin/categoryController')
 const bankController = require('../controllers/admin/bankController')
+const itemController = require('../controllers/admin/itemController')
 
 router.get('/', dashboardController.index)
 router.get('/categories', categoryController.index)
 router.get('/banks', bankController.index)
+router.get('/items', itemController.index)
 
 module.exports = router
