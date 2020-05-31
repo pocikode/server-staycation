1045a8f4eed069e93e0dbf20a48c365f0707aa64
Agus Supriyatna <agus@anterin.id>
2020-05-31 20:43:04 +0700

    admin dashboard controller

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var nunjucts = require('nunjucks')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbo
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
