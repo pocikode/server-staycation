85dfbfaaa12a9273ee7e17c2c395f5317e11b395
Agus Supriyatna <agus@anterin.id>
2020-06-01 21:01:17 +0700

    categories select in view add item

--- controllers/admin/itemController.js
+++ controllers/admin/itemController.js
@@ -1,5 +1,10 @@
+const Category = require('../../models/Category')
+
 module.exports = {
-  index: (req, res) => {
-    res.render('admin/item/index')
+  index: async (req, res) => {
+    let categories = await Category.find()
+    res.render('admin/item/index', {
+      categories
+    })
   }
 }
